296e9895391717a04aa680107bc1254a0cfc9c0a
Edison Mejia <atllion@hotmail.com>
2023-03-31 23:29:40 +0200

    Apariencia aplicación V2

--- src/Components/Productos/Producto.js
+++ src/Components/Productos/Producto.js
@@ -47,63 +47,37 @@ function Producto(props) {
             <div id='container'>
                 <img src='https://mdbootstrap.com/img/new/slides/041.webp' width="120" height="250" />
             </div>
-
             <div className='producto__nombre'>
                 <h2>{nombre}</h2>
                 <div className='producto__precio'>{precio} €</div>
             </div>
-            
             <Button variant="warning" onClick={handleShow}>
                 Ver detalles
             </Button>
             <Button variant="danger" onClick={borraHandler} ><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-</svg> 2</Button> <Button variant="info">AÑADIR</Button>
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </svg> 2</Button> <Button variant="info">COMPRAR</Button>
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{nombre}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>DETALLES DE MI PRODUCTO: {precio}</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Cancelar
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        OKk
-                    </Button>
-                </Modal.Footer>
+                <Modal.Body>Descripcion: {precio}</Modal.Body>
             </Modal>
             <div className='producto2'>
-
-
-
                 <Form onSubmit={submitHandler}>
                     <Container>
                         <Row>
                             <Col>
-                                <Button variant="primary" onClick={handleClose}>
-                                    +
-                                </Button>
-                            
-                                <Form.Control onChange={enviarCarritoHandler} type='text' value={25+" unidades"}  />
-                            
-
-                               
-                                <Button variant="primary" onClick={handleClose}>
-                                    -
-                                </Button>
-                                </Col>
+                                <Button variant="primary" onClick={handleClose}>+</Button>
+                                <Form.Control onChange={enviarCarritoHandler} type='text' value={25 + " unidades"} />
+                                <Button variant="primary" onClick={handleClose}>—</Button>
+                            </Col>
                             <Col>
-                                
-                            </Col>    
+                            </Col>
                         </Row>
                     </Container>
                 </Form>
-
-
-
-
             </div>
         </div>
     )
