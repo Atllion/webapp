ec50f8b58f78e70b3d6509693008d6cbc9c90d44
Edison Mejia <atllion@hotmail.com>
2023-04-03 12:39:17 +0200

    Añadir productos y precio

--- src/Components/Productos/Producto.js
+++ src/Components/Productos/Producto.js
@@ -11,13 +11,27 @@ import Col from 'react-bootstrap/Col';
 function Producto(props) {
 
     const [nombre, setNombre] = useState(props.producto.nombre);
-
-    //let nombre = props.producto.nombre;
-    const fecha = props.producto.fecha;
+    const [resultado, setResultado] = useState(0);
+    const [numero1, setNumero1] = useState(0);
     const precio = props.producto.precio;
 
     const clickHandler = () => {
         setNombre('Nuevo nombre');
+    }
+
+    const sumarHandler = () => {
+        setResultado(numero1+1);
+        setNumero1(numero1+1);
+        console.log(resultado);
+        
+    }
+    const restarHandler = () => {
+        if (resultado>=1){
+            setResultado(numero1-1);
+            setNumero1(numero1-1);
+            console.log(resultado);
+        }
+
     }
 
     const borraHandler = () => {
@@ -31,16 +45,7 @@ function Producto(props) {
 
     const submitHandler = (event) => {
         event.preventDefault();
-        const producto = {
-            id: Math.random().toString(),
-            nombre: nombre,
-            precio: precio,
-            fecha: new Date(fecha)
-        }
-        props.addProducto(producto);
-
     }
-
 
     return (
         <div className='producto'>
@@ -51,13 +56,11 @@ function Producto(props) {
                 <h2>{nombre}</h2>
                 <div className='producto__precio'>{precio} €</div>
             </div>
-            <Button variant="warning" onClick={handleShow}>
-                Ver detalles
-            </Button>
+            <Button variant="warning" onClick={handleShow}> Ver detalles </Button>
             <Button variant="danger" onClick={borraHandler} ><svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-            </svg> 2</Button> <Button variant="info">COMPRAR</Button>
-
+            </svg> {resultado}  </Button>
+            <Button variant="info">COMPRAR</Button>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{nombre}</Modal.Title>
@@ -69,11 +72,9 @@ function Producto(props) {
                     <Container>
                         <Row>
                             <Col>
-                                <Button variant="primary" onClick={handleClose}>+</Button>
-                                <Form.Control onChange={enviarCarritoHandler} type='text' value={25 + " unidades"} />
-                                <Button variant="primary" onClick={handleClose}>—</Button>
-                            </Col>
-                            <Col>
+                                <Button variant="primary" onClick={sumarHandler}>+</Button>
+                                <Form.Control onChange={enviarCarritoHandler} type='text' value={resultado + " unidades = "+resultado*precio +" €"} />
+                                <Button variant="primary" onClick={restarHandler}>—</Button>
                             </Col>
                         </Row>
                     </Container>
@@ -82,5 +83,4 @@ function Producto(props) {
         </div>
     )
 }
-
 export default Producto;
